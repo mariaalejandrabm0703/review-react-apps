0f9140614575b0d7333258a24a95191f8d4bb772
maria.bohorquez <maria.bohorquez@ceiba.com.co>
2023-02-28 15:36:37 -0500

    02-card-twitter add few twitter accounts to render

--- 02-card-twitter/src/App.jsx
+++ 02-card-twitter/src/App.jsx
@@ -1,30 +1,36 @@
 import { Card } from './components/Card/Card';
 
-const user = {
-  userName: 'midudev',
-  name: 'Miguel Ángel Durán',
-  isFollowing: true
-};
+const user = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'maraBohrquez19',
+    name: 'María Bohórquez',
+    isFollowing: false
+  },
+  {
+    userName: 'andres_vernazza',
+    name: 'Andres Vernazza',
+    isFollowing: true
+  }
+]
 
 function App() {
 
   return (
     <section className='App'>
-      <Card 
-        key={user.userName}
-        userName={user.userName}
-        initialIs={user.isFollowing}
-      >
-        {user.name}
-      </Card> 
-      
-      <Card 
-      key={user.userName}
-      userName={user.userName}
-      initialIs={false}
-    >
-      {user.name}
-      </Card> 
+      {
+        user.map(user => (
+          <Card
+            key={user.userName}
+            userName={user.userName}
+            initialIs={user.isFollowing}
+          >{user.name}</Card>
+        ))
+      }
     </section>
   )
 }
