a3e980c730f1ffd90462f52bb94a8f10a27d7f9b
maria.bohorquez <maria.bohorquez@ceiba.com.co>
2023-03-03 16:07:11 -0500

    03-react-prueba-tecnica fetch data in efect

--- 03-react-prueba-tecnica/App.jsx
+++ 03-react-prueba-tecnica/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 export function App () {
     const [fact, setfact] = useState('lorem ipsum dolor sit amet, consectetur adipiscing')
 
+    useEffect(() => {
+      fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(rest =>rest.json())
+      .then(data => setfact(data.fact));
+    }, [])
+
   return (
        <main>
           <h1>App de gatitos</h1>
-         <p>{fact}</p>
+         { fact && <p>{fact}</p>}
        </main>
     )
 }
