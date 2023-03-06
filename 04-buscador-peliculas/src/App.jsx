5f63875e696179cfcc85f62c14544f58fb64c3c3
maria.bohorquez <maria.bohorquez@ceiba.com.co>
2023-03-05 21:20:34 -0500

    04-buscador-peliculas add form and list of movies

--- 04-buscador-peliculas/src/App.jsx
+++ 04-buscador-peliculas/src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import responseMovies from './mocks/results.json';
+
+const OMDB_API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f810fa4e'
+// https://www.omdbapi.com/?i=tt3896198&apikey=f810fa4e&s=marvels
 
 function App() {
-  const [count, setCount] = useState(0)
+  const movies = responseMovies.Search;
+  const hasMovie = movies?.length > 0;
 
   return (
-    <div className="App">
-      <h1>Hola para buscar peliculas</h1>
+    <div className="container">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Start Wars, The Matrix' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        {hasMovie ?
+          <ul>
+            {movies.map(movie => (
+              <li key={movie.imdbID}>
+                <a target="_blank" href={`https://www.imdb.com/title/${movie.imdbID}`}>
+                  <h3>{movie.Title}</h3>
+                </a>
+                <p>Año: {movie.Year}</p>
+                <p>Tipo: {movie.Type}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+          :
+          <p>No hay peliculas</p>
+        }
+      </main>
     </div>
   )
 }
