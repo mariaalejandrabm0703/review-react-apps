312bff3f216a186e530d37ea3ec56ab01bc765b7
maria.bohorquez <maria.bohorquez@ceiba.com.co>
2023-03-06 19:17:26 -0500

    04-buscador-peliculas using state to get value of form

--- 04-buscador-peliculas/src/App.jsx
+++ 04-buscador-peliculas/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { Movies } from './components/Movies';
 import { useMovies } from './hooks/useMovies';
@@ -8,23 +8,58 @@ const OMDB_API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f810fa4e'
 
 function App() {
   const movies = useMovies();
-  const hasMovie = movies?.length > 0;    
+  const hasMovie = movies?.length > 0;
+  const [movie, setMovie] = useState('');
+  const [error, setError] = useState('');
+  const isFirstInput = useRef(true)
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const { movie } = Object.fromEntries(new window.FormData(event.target));
     console.log(movie);
-    if(movie === '') return
   }
+
+  const handleChangeMovie = (event) => {
+    setMovie(event.target.value);
+  }
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = movie === ''
+      return
+    }
+
+    if (movie === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (movie.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (movie.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [movie])
+
+
 
   return (
     <div className="container">
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input name="movie" placeholder='Avengers, Start Wars, The Matrix' />
+          <input style={{
+              border: '1px solid gray',
+              borderColor: error ? 'red' : 'gray'
+            }} value={movie} onChange={handleChangeMovie} placeholder='Avengers, Start Wars, The Matrix' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
         {hasMovie ?
@@ -38,4 +73,3 @@ function App() {
 }
 
 export default App
-    
